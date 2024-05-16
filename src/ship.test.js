f2a8b0c87f701a2766f18f1152f985f60d286a21
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-05-16 11:38:22 +0800

    add orientation property to test

--- src/ship.test.js
+++ src/ship.test.js
@@ -9,10 +9,10 @@ goo.hit();
 
 test("return object", () => {
   expect(foo.getShip()).toEqual({
-    ship: [0, 0],
     length: 2,
     hitCount: 2,
     sunk: true,
+    orientation: "horizontal",
   });
 });
 
