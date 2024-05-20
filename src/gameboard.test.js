58b89d00657bdf68e8ebfb690dc2a1d4884c193e
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-05-20 16:47:02 +0800

    Test: receive attack, return misses

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -3,6 +3,7 @@ import { Ship } from "./ship";
 
 let board = Gameboard();
 const ship = Ship(2);
+
 board.createBoard();
 board.placeShip(2, 3, ship);
 
@@ -22,4 +23,25 @@ test("place ship", () => {
     sunk: false,
     orientation: "horizontal",
   });
+});
+
+test("attack ship", () => {
+  board.receiveAttack(2, 3);
+  expect(board.getBoard()[2][3].getShip().hitCount).toBe(1);
+});
+
+test("miss an attack", () => {
+  board.receiveAttack(2, 5);
+  expect(board.getMisses()).toEqual([[2, 5]]);
+});
+
+test("get hit count", () => {
+  let board = Gameboard();
+  let foo = board.getShips().foo;
+
+  board.createBoard();
+  board.placeShip(1, 2, foo);
+  board.receiveAttack(1, 2);
+
+  expect(board.getShips().foo.getShip().hitCount).toBe(1);
 });
