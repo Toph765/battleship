5e0d1622be6b06236c061d8b293facfa87d9896e
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-05-31 14:15:38 +0800

    Render board with ships

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import { renderBoard } from "./DOM";
 
 let playerOne = Player("One");
 let playerTwo = Player("Two");
+let playerOneShips = playerOne.board.getShips();
+let foo = playerOneShips.s_ship_I;
 
 playerOne.board.createBoard();
 playerTwo.board.createBoard();
+
+playerOne.board.placeShip(0, 1, foo);
 
 renderBoard(playerOne);
 renderBoard(playerTwo);
