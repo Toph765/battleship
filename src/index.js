e56e90cc7e29ceab0c140cdcd5be452de4ae64f9
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-09-07 15:16:33 +0800

    pvpBtn, pvcBtn: add event listeners

--- src/index.js
+++ src/index.js
@@ -3,27 +3,56 @@ import {
   renderBoard,
   grabPlayerShips,
   initPlaceShip,
-  renderForm,
   initFinSetup,
   initplayBtn,
+  compMode,
 } from "./DOM";
 
-let playerOne = Player("One");
-let playerTwo = Player("Two");
+const gameMode = document.querySelector(".game_mode");
+const pvpBtn = document.querySelector("#pvp");
+const pvcBtn = document.querySelector("#pvc");
+const playerOne = Player("One");
+const playerTwo = Player("Two");
 
-renderBoard(playerOne);
-renderBoard(playerTwo);
+pvpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderBoard(playerOne);
+  renderBoard(playerTwo);
 
-renderForm(playerOne);
-renderForm(playerTwo);
+  grabPlayerShips(playerOne);
+  grabPlayerShips(playerTwo);
 
-grabPlayerShips(playerOne);
-grabPlayerShips(playerTwo);
+  initPlaceShip(playerOne);
+  initPlaceShip(playerTwo);
 
-initPlaceShip(playerOne);
-initPlaceShip(playerTwo);
+  initFinSetup(playerOne);
+  initFinSetup(playerTwo);
 
-initFinSetup(playerOne);
-initFinSetup(playerTwo);
+  initplayBtn(playerOne, playerTwo);
 
-initplayBtn(playerOne, playerTwo);
+  return gameMode.close();
+});
+
+pvcBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const main = document.querySelector("main");
+  const form = document.querySelector("#playerTwo-form");
+
+  renderBoard(playerOne);
+  renderBoard(playerTwo);
+  grabPlayerShips(playerOne);
+  initPlaceShip(playerOne);
+  initFinSetup(playerOne);
+  initFinSetup(playerTwo);
+  initplayBtn(playerOne, playerTwo);
+
+  compMode(playerTwo);
+
+  form.setAttribute("style", "display: none;");
+  main.classList.add("pvc");
+
+  return gameMode.close();
+});
+
+document.addEventListener("onload", gameMode.showModal());
